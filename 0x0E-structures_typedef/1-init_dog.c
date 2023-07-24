8c7116a6869cfabff21c66f9b865493d1101f8e0
izzimars <israelmacaulay8@gmail.com>
2023-07-23 19:16:00 -0700

    hmm

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * init_dog- Entry point
@@ -12,6 +14,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Short description
  * @name: First member
@@ -18,3 +20,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
