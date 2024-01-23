cb159f6e6acc47594c27a89452a52363afda0a4f
Macaulay Israel <israelmacaulay8@gmail.com>
2024-01-23 11:37:42 -0800

    0X0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
+	int j, k, l;
 
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
+	{
+		free(p);
 		return (NULL);
-	p->name = name;
+	}
+	for (j = 0; name[j]; j++)
+		;
+	for (k = 0; owner[k]; k++)
+		;
+	p->name = malloc(sizeof(char) + j + 1);
+	p->owner = malloc(sizeof(char) + k + 1);
+	if (!(p->name) || !(p->owner))
+	{
+		free(p->owner);
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
+	for (j = 0; l < j; l++)
+		p->name[l] = name[l];
+	p->name[l] = '\0';
+	for (l = 0; l < k; l++)
+		p->name[l] = name[l];
+	p->name[l] = '\0';
 	p->age = age;
-	p->owner = owner;
 	return (p);
 }
