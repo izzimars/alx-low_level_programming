eabe7c31cf3707d6647f7d1750b8e82611029731
Macaulay Israel <israelmacaulay8@gmail.com>
2024-01-25 17:36:25 -0800

    0X10 trial-7

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
 #ifndef _VARAIADIC_FUNCTIONS_
 #define _VARIADIC_FUNCTIONS_
 
-#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
