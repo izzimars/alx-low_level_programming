bfffc28adece65d1db7cd06c543c586713e23a4a
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-01 18:40:47 -0800

    0x12 task-0 fix-1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
-
-	nelem = 0;
+	size_t i;
+	
+	i = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d]  %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		i++;
 	}
-	return (nelem);
+	return (i);
 }
