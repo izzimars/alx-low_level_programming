93246bb66727ae76d3dfe0b02b2b78aed80029c1
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-01 18:57:48 -0800

    0x12 task-1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list- Entry point
+ * Description: 'the program's description'
+ * @h: First operand
+ *
+ * Return: An int
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
