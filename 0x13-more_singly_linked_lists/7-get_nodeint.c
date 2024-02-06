995013de2268087d11279e3d0e11c61c571b73ee
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-06 08:16:13 -0800

    0X13 trial-7 fix-0

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - frees a list
+ * @head: head of the linked list.
+ * @index: An integer parameter
+ *
+ * Return: no return.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	unsigned int n = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	current = head;
+	while ((n <= index) && (current != NULL))
+	{
+		if (n == index)
+			return (current);
+		current = current->next;
+		++n;
+	}
+	free(current);
+	return (NULL);
+}
