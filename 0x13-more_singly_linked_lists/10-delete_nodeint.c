a84e9f48448476c2f3401751b7c3df731e63ddd6
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-07 09:57:47 -0800

    0x13 trial-10 fix-10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,42 +1,46 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index- Entry point
- * Description: 'the program's description'
- * @head: First operand
- * @index: Second parameter
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
  *
- * Return: An int
+ * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *prev, (*current);
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
 
-	if (*head == NULL)
-		return (-1);
-	current = *head;
+	prev = *head;
+
 	if (index != 0)
 	{
-		for (i = 0; i < index && current != NULL; i++)
+		for (i = 0; i < index - 1 && prev != NULL; i++)
 		{
-			prev = current;
-			current = current->next;
+			prev = prev->next;
 		}
 	}
-	if (current == NULL && index != 0)
+
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
 		return (-1);
-	if (index == 0 && (current->next != NULL))
-	{
-		*head = current->next;
 	}
-	else if (index == 0 && (current->next == NULL))
+
+	next = prev->next;
+
+	if (index != 0)
 	{
-		*head = NULL;
+		prev->next = next->next;
+		free(next);
 	}
 	else
 	{
-		prev->next = current->next;
+		free(prev);
+		*head = next;
 	}
 
 	return (1);
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,51 @@
 #include "lists.h"
 
 /**
- * pop_listint - frees a list
- * @head: head of the linked list.
- * Return: no return.
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
-
-int pop_listint(listint_t **head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, (*prev);
-	int n;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-	if (head == NULL)
+	h = *head;
+
+	if (idx != 0)
 	{
-		return (0);
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
 	}
-	current = *head;
-	n = (*head)->n;
-	prev = (*head)->next;
-	free(current);
-	*head = prev;
-	return (n);
+
+	if (h == NULL && idx != 0)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
+
+	return (new);
 }
