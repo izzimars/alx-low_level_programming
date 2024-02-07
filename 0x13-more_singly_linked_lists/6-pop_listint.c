988fd1ef58ad756526d829ca4f9a60200b94c46b
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-07 10:52:24 -0800

    0x13 trial-10 fix-12

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,51 +1,24 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node
- * at a given position.
- * @head: head of a list.
- * @idx: index of the list where the new node is
- * added.
- * @n: integer element.
- *
- * Return: the address of the new node, or NULL if it
- * failed.
+ * pop_listint - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
+int pop_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *h;
+	listint_t *current, (*prev);
+	int n;
 
-	h = *head;
-
-	if (idx != 0)
+	if (*head == NULL)
 	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
-		{
-			h = h->next;
-		}
+		return (0);
 	}
-
-	if (h == NULL && idx != 0)
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = h->next;
-		h->next = new;
-	}
-
-	return (new);
+	current = *head;
+	n = (*head)->n;
+	prev = (*head)->next;
+	free(current);
+	*head = prev;
+	return (n);
 }
