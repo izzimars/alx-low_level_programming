5830dd3fc87967465062878e5f3a1063cb747bbe
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-13 09:49:32 -0800

    0X15 trial-1 fix-2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	source_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 644);
+	source_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 	if (source_fd == -1)
 		return (-1);
 	if (!text_content)
