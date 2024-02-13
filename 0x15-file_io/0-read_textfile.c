e33b88b014b0ad775240e7ad293455f124aafdf1
Macaulay Israel <israelmacaulay8@gmail.com>
2024-02-13 08:34:58 -0800

    0X15 trial-0 fix-0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,34 +11,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytes_read, bytes_written;
-	int source_fd, n = 0;
-	char buffer[1024];
+	int source_fd;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	source_fd = open(filename, O_RDONLY);
 	if (source_fd == -1) 
-		return (-1);
-	while (n < (int)letters)
-	{
-		bytes_read = read(source_fd, buffer + n, 1);
-		if (bytes_read == -1) 
-		{
-			close(source_fd);
-			return (-1);
-		}
-		if (bytes_read == 0)
-			break;
-		if (n == 1023)
-		{
-			bytes_written = write(1, buffer, n);
-			n = 0;
-		}
-		++n;
-	}
-	bytes_written = write(1, buffer, n);
+		return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+	bytes_read = read(source_fd, buffer, letters);
+	if (bytes_read == -1)
+		return (0);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	close(source_fd);
 	if (bytes_written == -1)
-		return (-1);
+		return (0);
+	free(buffer);
 	return (bytes_written);
 }
