2c2fb4800b4bbaa1928f296540047a95e9e61310
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-21 15:41:11 -0700

    0x17 tk 07 fix 00

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Add an element of dlistint at
+ * the argument index position in the dlistint_t list
+ *
+ * @h: head of the list
+ * @idx: The int data
+ * @n: The int data
+ * Return: Address of the added data
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *cur;
+	dlistint_t *new;
+	dlistint_t *pre;
+	unsigned int i = 0;
+
+	if (h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t *));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	cur = *h;
+	while (cur != NULL)
+	{
+		if (i == idx)
+		{
+			pre->next = new;
+			new->prev = pre;
+			new->next = cur;
+			return (new);
+		}
+		pre = cur;
+		cur = cur->next;
+		i++;
+	}
+	return (NULL);
+}
