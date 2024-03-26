e3973ba326e8800348fb909f889c721c26ba59d1
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-25 20:10:34 -0700

    0x02 tk 09 fix 00

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,44 +2,51 @@
 #include <stdio.h>
 
 /**
- * _islower- Entry point
+ * times_table- Entry point
  * Description: 'the program's description'
- * @c: First operand
  *
  * Return: Always 0 (Success)
  */
 void times_table(void)
 {
-	int i,j,k,l;
+	int i, j, l;
 
 	i = 0;
 	j = 0;
 	while (2 > 1)
 	{
 		l = i * j;
-		++j;
-		if (l < 10)
+		if (j == 0)
 		{
 			_putchar(l + '0');
-			_putchar(44);
+		}
+		else
+		{
 			_putchar(32);
+			if (l < 10)
+			{
+				_putchar(32);
+				_putchar(l + '0');
+			}
+			else if (l >= 10)
+			{
+				_putchar((l / 10)  + '0');
+				_putchar((l % 10) + '0');
+			}
 		}
-		if (l > 10)
+		if (j == 9)
 		{
-			k = l / 10;
-			l = l % 10;
-			_putchar(k + '0');
-			_putchar(l + '0');
-			_putchar(44);
-		}
-		_putchar(32);
-		if (j > 9)
-		{
+			if (i == 9)
+			{
+				_putchar('\n');
+				break;
+			}
 			j = 0;
-		        ++i;
+			i++;
 			_putchar('\n');
+			continue;
 		}
-		if (i > 9)
-			return;
+		_putchar(44);
+		j++;
 	}
 }
