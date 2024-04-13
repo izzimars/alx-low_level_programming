940797eccbcbfb1cddae3bb30bab3cbdfcbb4a19
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-13 15:52:00 -0700

    0x1A tk 00 fix 00

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,10 +2,9 @@
 #include <stddef.h>
 
 /**
- * hash_table_t- Entry point
+ * hash_table_create- Entry point
  * Description: 'the program's description'
- * @name: First operand
- * @f : Second operand
+ * @size: First operand
  *
  * Return: A pointer of type char
  */
@@ -15,9 +14,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
+		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
-	return table;
+	return (table);
 }
